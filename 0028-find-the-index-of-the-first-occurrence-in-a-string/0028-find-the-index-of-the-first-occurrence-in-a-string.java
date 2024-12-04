f350b3f7da4fa9658501046d0e783cd4858d419a
onDayLight <naoldana5@gmail.com>
2024-12-04 15:50:37 +0300

    Time: 0 ms (100.00%), Space: 41.6 MB (26.80%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+       
+        for(int i=0;i<haystack.length()-needle.length()+1;i++)
+        {  
+            if(needle.charAt(0)==haystack.charAt(i) )
+                if(needle.equals(haystack.substring(i,i+needle.length())))
+                    return i;
+        }
+        return -1;
+    }
+}
