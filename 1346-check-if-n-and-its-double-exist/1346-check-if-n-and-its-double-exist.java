05bcc8c582563e133509053a63c4926e0bcc7e94
onDayLight <naoldana5@gmail.com>
2024-12-21 18:38:20 +0300

    Time: 2 ms (90.45%), Space: 43.1 MB (50.75%) - LeetHub

---
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        for(int i=0;i<arr.length;i++)
+        { for(int j=0;j<arr.length;j++)
+        {   if( i==j)
+              continue;
+            else if(arr[j]*2==arr[i])
+                return true;
+        }    
+        }
+        return false;
+    }
+}
